79dabe5c27976697953bb28dc6e9979322d11d42
yisus44 <jesusadrian1953.1@gmail.com>
2023-08-18 22:05:57 -0600

    Added example of bad request

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './entity/user.entity';
 
 @Injectable()
 export class UsersService {
   async createUser(username: string, password: string): Promise<User> {
+    // if (username) throw new BadRequestException('Username must not be empty');
     return {
       id: 1,
       password,
