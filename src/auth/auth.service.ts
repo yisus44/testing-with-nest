15f698089a4b38de48b35490141010ce2e8ccf5c
yisus44 <jesusadrian1953.1@gmail.com>
2023-08-21 16:14:36 -0600

    added condition for when the username is null in the register user at the auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,8 +7,7 @@ export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
   async registerUser(username: string, password: string): Promise<User | null> {
-    //if (username) return null;
-    // if (!username) return null;
+    if (username) return null;
 
     const user = await this.usersService.createUser(username, password);
     return user;
